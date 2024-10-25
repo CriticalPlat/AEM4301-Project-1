c5b4a54f98a28df94d5547e211195b143b0de9a1
C. Lani <calde241@umn.edu>
2024-10-25 09:53:19 -0500

    I forgor to add code
    here :D

---
+++ Project_1.m
@@ -0,0 +1,19 @@
+%% Define f(x), f'(x)=g(x)
+
+relerr=1; %to get started
+tol=.01; %tolerance
+xi=4; %initial guess
+n=0; %number of iterations
+nmax=20;
+
+while relerr>tol
+    xi1=xi-f(xi)/g(xi); %get new x
+    relerr=abs((xi1-xi)/xi); %relative error
+    xi=xi1;
+
+    n=n+1;
+    if n>nmax
+        break;
+    end
+end
+
