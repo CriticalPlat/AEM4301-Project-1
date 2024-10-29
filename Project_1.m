3b0ded00506a7ea0a619f3539ca95c47bd27488e
roble146m <roble146@umn.edu>
2024-10-29 15:32:49 -0500

    Added eqns

--- Project_1.m
+++ Project_1.m
@@ -1,20 +1,7 @@
-%% Define f(x), f'(x)=g(x)
-
-relerr=1; %to get started
-tol=.01; %tolerance
-xi=4; %initial guess
-n=0; %number of iterations
-nmax=20;
-
-while relerr>tol
-    xi1=xi-f(xi)/g(xi); %get new x
-    relerr=abs((xi1-xi)/xi); %relative error
-    xi=xi1;
-
-    n=n+1;
-    if n>nmax
-        break;
-    end
-end
-
-% Trial Change
+mu = 1;
+R0 = [0.5 0.6 0.7];
+R1 = [0 1 0];
+dt = 0.9667663;
+z0 = 4;
+dir = 0;
+[V1,V2] = Lamberts(1, R0, R1, dt, 4, 0);clc
