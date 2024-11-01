5b8bd10827c3a42a0cca4e5b94bdd7537c586d0a
C. Lani <calde241@umn.edu>
2024-11-01 07:50:44 -0500

    Hopefully done!

--- Project_1.m
+++ Project_1.m
@@ -1,20 +1,71 @@
-%% Define f(x), f'(x)=g(x)
+%% Project Main
+clear;
+clc;
+% Problem 1 Long Way
+mu = 1;
+R0 = [0.5 0.6 0.7];
+R1 = [0 1 0];
+dt = 0.9667663;
+z0 = 0;
+dir = 0;
+Lamberts(mu, R0, R1, dt, z0, dir);
+disp('Problem 1 Long Way ')
+disp('V1:'), disp(V1)
+disp('V2:'), disp(V2)
 
-relerr=1; %to get started
-tol=.01; %tolerance
-xi=4; %initial guess
-n=0; %number of iterations
-nmax=20;
+%% Lambert's Function
+function [V1,V2] = Lamberts(mu, R0, R1, dt, z0, dir)
 
-while relerr>tol
-    xi1=xi-f(xi)/g(xi); %get new x
-    relerr=abs((xi1-xi)/xi); %relative error
-    xi=xi1;
+    r0 = norm(R0); % length of R0
+    r1 = norm(R1); % length of R1
+    dtheta = acos(dot(R0, R1) / r0 / r1); %transfer angle
 
-    n=n+1;
-    if n>nmax
-        break;
+    if dir == 1 %short way
+        sgn = 1; %"direction of motion"
+    else
+        dtheta = 2*pi - dtheta; %long way
+        sgn = -1;
     end
-end
+    
+    A = sgn * sqrt(r0 * r1 * (1 + cos(dtheta)));
+    z = z0; %trial value
+    relerr = 1; %initialize error
+    tol = 1e-7; % Accepted relative error
+    n = 0;
+    nmax = 200;
+    while relerr > tol && n < nmax
+        if z == 0 %to avoid division by zero
+            S = 1/6; Sprime = -1/120;
+            C = 1/2; Cprime = -1/24;
+        else
+            C = (1 - cos(sqrt(z))) / z;
+            S = (sqrt(z) - sin(sqrt(z))) / z^(3 / 2);
+            Cprime = (1 / (2 * z)) * (1 - z * S - 2 * C);
+            Sprime = (1 / (2 * z)) * (C - 3 * S);
+        
+        end
+            %calculate u(z),v(z), new value of z
+            
+            y = r0 + r1 - A * ((1-z*S) / (sqrt(C)));
+            chi = sqrt(y / C);
+            u = (1/sqrt(mu)) * ((chi^3)*S + A*sqrt(y)) - dt;
+            v = (1/sqrt(mu)) * ((chi^3) * (Sprime-(3*S*Cprime)/(2*C)) + (A/8) * ((3*S*sqrt(y))/C + A/chi));
+            % Just Added
+            z_new = z - u / v;
+            relerr = abs((z_new - z) / z);
+            z = z_new;
+            n = n + 1;
 
-% Trial Change
+    end
+        if n>nmax
+        end
+    
+        %use value of z obtained to determine v1,v2
+            f = 1 - (chi^2)*C / r0;
+            g = dt - (chi^3)*S / sqrt(mu); % = fp
+            f_prime = ((sqrt(mu))/(r1*r0))*chi*(z*S-1);
+            g_prime = (1 + g*f_prime) / f;
+            V1 = (R1 - f*R0) / g;
+            V2 = f_prime*R0 + g_prime*V1;
+        
+end
